25a980231d1eb33ad668662d9c16ad38918df6e5
jiyongd <yuemiyuta@gmail.com>
2020-01-17 20:27:36 +0900

    Sync Git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
 import './App.css';
 
+
 class App extends Component {
+  constructor(props){ //컴포넌트가 실행 될 때 초기화 담당
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:"world wide web!"},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for impormation'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'HTML is for interactive'}
+      ]
+    }
+  }
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        <Subject title="React" sub="For UI"></Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
   }
