e50f03893275f121a301d11a6ce9e96b0d760652
jiyongd <yuemiyuta@gmail.com>
2020-01-27 01:44:43 +0900

    Event (preventDefault, .bind)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
   constructor(props){ //컴포넌트 초기화
     super(props);
     this.state={
+      mode:'read',
       subject:{title:'WEB', sub:'World Wide Web'},
+      welcome:{title:'welcome', desc:'Hello React!!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for design'},
@@ -17,11 +19,30 @@ class App extends Component {
     }
   }
   render(){   //렌더 메소드
+    var _title, _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
-        <Subject title={ this.state.subject.title } sub={ this.state.subject.sub }></Subject>
+        {/*<Subject title={ this.state.subject.title } sub={ this.state.subject.sub }></Subject>*/}
+        <header>
+            <h1><a href="/" onClick={function(e){   //이벤트 실행 함수 안에서 this는 값이 없다. undefined!!
+              console.log(e);
+              e.preventDefault();
+              //this.state.mode = 'welcome';
+              this.setState({
+                mode:'welcome'
+              })
+            }.bind(this)}>{ this.state.subject.title }</a></h1>
+            { this.state.subject.sub }
+        </header>
         <TOC data={ this.state.contents }></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Content title={ _title } desc={ _desc }></Content>
       </div>
     );
   }
